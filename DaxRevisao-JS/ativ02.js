37caf4f59e7768e4b9a32e1b5e03c6e243698d76
Camila Paranhos <milladecastro1309@gmail.com>
2025-07-28 09:55:40 -0300

    Update ativ02.js

--- DaxRevisao-JS/ativ02.js
+++ DaxRevisao-JS/ativ02.js
@@ -1,7 +1,7 @@
 let numero = 3;
 
 if (numero % 2 === 0) {
-    console.log("O número é par.");
+    console.log("O número", numero,  "é par.");
 } else {
-    console.log("O número é ímpar.");
+    console.log("O número" , numero,  "é ímpar.");
 }
